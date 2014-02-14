1dcf9793d8df8354df1e995c1c7de41fdd5b6e29
Kevin Olsthoorn <info@kevinolsthoorn.nl>
2014-02-14 11:19:32 +0100

    Updated comments to explain NSLog line.

--- HellGlow World/HellGlow World/main.m
+++ HellGlow World/HellGlow World/main.m
@@ -13,7 +13,7 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
+        // Log the string HellGlow, World!
         NSLog(@"HellGlow, World!");
         
     }
