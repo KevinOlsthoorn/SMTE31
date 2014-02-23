71655d91d0da5243aa023f70dfb46b6bc470ce4e
Kevin Olsthoorn <info@kevinolsthoorn.nl>
2014-02-23 15:16:41 +0100

    Added DetailsViewController to link with the Details View Controller.

---
+++ Week 2/Kevin's App/Kevin's App/DetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.h
+//  Kevin's App
+//
+//  Created by Kevin Olsthoorn on 20-02-14.
+//  Copyright (c) 2014 Kevin Olsthoorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+
+@end
---
+++ Week 2/Kevin's App/Kevin's App/DetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetailsViewController.m
+//  Kevin's App
+//
+//  Created by Kevin Olsthoorn on 20-02-14.
+//  Copyright (c) 2014 Kevin Olsthoorn. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+
+@end
+
+@implementation DetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
