02f492e37872ce350e6c9903fc71cf709417989f
Kevin Olsthoorn <info@kevinolsthoorn.nl>
2014-02-23 15:15:19 +0100

    Added GeneralViewController to link with the General View.

---
+++ Week 2/Kevin's App/Kevin's App/GeneralViewController.h
@@ -0,0 +1,13 @@
+//
+//  GeneralViewController.h
+//  Kevin's App
+//
+//  Created by Kevin Olsthoorn on 20-02-14.
+//  Copyright (c) 2014 Kevin Olsthoorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GeneralViewController : UIViewController
+
+@end
