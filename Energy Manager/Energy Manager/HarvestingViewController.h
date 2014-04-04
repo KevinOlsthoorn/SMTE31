3b91d5d8a96fa7c50e28e62b10ff950f493a9695
Kevin Olsthoorn <info@kevinolsthoorn.nl>
2014-04-04 21:20:36 +0200

    Added harvesting elements to the iPad storyboard. Added IBOutlets for the elements on the harvesting view. Added ParseJson function to HarvestingViewController.m.

--- Energy Manager/Energy Manager/HarvestingViewController.h
+++ Energy Manager/Energy Manager/HarvestingViewController.h
@@ -10,4 +10,25 @@
 
 @interface HarvestingViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *EnergyLifetime;
+@property (weak, nonatomic) IBOutlet UILabel *PowerPeakLifetime;
+@property (weak, nonatomic) IBOutlet UILabel *EnergyToday;
+@property (weak, nonatomic) IBOutlet UILabel *PowerPeakToday;
+
+@property (weak, nonatomic) IBOutlet UILabel *Input1Voltage;
+@property (weak, nonatomic) IBOutlet UILabel *Input1Current;
+@property (weak, nonatomic) IBOutlet UILabel *Input1Power;
+
+@property (weak, nonatomic) IBOutlet UILabel *Input2Voltage;
+@property (weak, nonatomic) IBOutlet UILabel *Input2Current;
+@property (weak, nonatomic) IBOutlet UILabel *Input2Power;
+
+@property (weak, nonatomic) IBOutlet UILabel *PowerIn;
+@property (weak, nonatomic) IBOutlet UILabel *PowerOut;
+@property (weak, nonatomic) IBOutlet UILabel *PowerEfficiency;
+
+@property (weak, nonatomic) IBOutlet UILabel *GridVoltage;
+@property (weak, nonatomic) IBOutlet UILabel *GridCurrent;
+@property (weak, nonatomic) IBOutlet UILabel *GridFrequency;
+
 @end
--- Energy Manager/Energy Manager/HarvestingViewController.m
+++ Energy Manager/Energy Manager/HarvestingViewController.m
@@ -27,12 +27,53 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    [self parseJson];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)parseJson
+{
+    NSData *jsonData = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:@"http://www.energy.xk140.nl/db_information.php?json=conv"]];
+    
+    NSError *error;
+    NSMutableDictionary *energyData = [NSJSONSerialization
+                                       JSONObjectWithData:jsonData
+                                       options:NSJSONReadingMutableContainers
+                                       error:&error];
+    
+    if (error)
+    {
+        NSLog(@"%@", [error localizedDescription]);
+    }
+    else
+    {
+        _EnergyLifetime.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"energylifetime"], @" kWh"];
+        _PowerPeakLifetime.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"powerpeak"], @" W"];
+        _EnergyToday.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"dailyenergy"], @" Wh"];
+        _PowerPeakToday.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"dailypowerpeak"], @" W"];
+        
+        _Input1Voltage.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"voltage1"], @" V"];
+        _Input1Current.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"current1"], @" A"];
+        _Input1Power.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"power1"], @" W"];
+        
+        _Input2Voltage.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"voltage2"], @" V"];
+        _Input2Current.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"current2"], @" A"];
+        _Input2Power.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"power2"], @" W"];
+        
+        _PowerIn.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"power"], @" W"];
+        _PowerOut.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"netpower"], @" W"];
+        _PowerEfficiency.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"efficiency"], @" %"];
+        
+        _GridVoltage.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"netvoltage"], @" W"];
+        _GridCurrent.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"netcurrent"], @" A"];
+        _GridFrequency.text = [NSString stringWithFormat:@"%@%@", [energyData objectForKey:@"netfrequency"], @" Hz"];
+    }
 }
 
 @end
