02b55ab354a5eb95d083e544b3f09c1770e8f632
Kevin Olsthoorn <info@kevinolsthoorn.nl>
2014-04-11 22:18:42 +0200

    Added more code for the core-plot graph (reading json information and displaying times from the feeds on the x-axis).

--- Energy Manager/Energy Manager/ConsumptionViewController.m
+++ Energy Manager/Energy Manager/ConsumptionViewController.m
@@ -14,6 +14,10 @@
 
 @implementation ConsumptionViewController
 
+NSData *graphData;
+NSArray *graphDataDictionary;
+NSMutableArray *xAxisDateLabels;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -27,6 +31,14 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    NSError *graphDataError;
+    graphData = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:@"http://www.energy.xk140.nl/json_data.php?type=cons"]];
+    graphDataDictionary = [NSJSONSerialization
+                                       JSONObjectWithData:graphData
+                                       options:kNilOptions
+                                       error:&graphDataError];
+    xAxisDateLabels = [[NSMutableArray alloc] init];
     
     // We need a hostview, you can create one in IB (and create an outlet) or just do this:
     CPTGraphHostingView* hostView = [[CPTGraphHostingView alloc] initWithFrame:_hostView.frame];
@@ -43,10 +55,6 @@
     // Get the (default) plotspace from the graph so we can set its x/y ranges
     CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *) graph.defaultPlotSpace;
     
-    // Note that these CPTPlotRange are defined by START and LENGTH (not START and END) !!
-    [plotSpace setYRange: [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat( 0 ) length:CPTDecimalFromFloat( 5 )]];
-    [plotSpace setXRange: [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat( 0 ) length:CPTDecimalFromFloat( 10 )]];
-    
     plotSpace.allowsUserInteraction = YES;
     
     // Create the plot (we do not define actual x/y values yet, these will be supplied by the datasource...)
@@ -57,6 +65,108 @@
     
     // Finally, add the created plot to the default plot space of the CPTGraph object we created before
     [graph addPlot:plot toPlotSpace:graph.defaultPlotSpace];
+    
+    // 1 - Create styles
+    CPTMutableTextStyle *axisTitleStyle = [CPTMutableTextStyle textStyle];
+    axisTitleStyle.color = [CPTColor whiteColor];
+    axisTitleStyle.fontName = @"Helvetica-Bold";
+    axisTitleStyle.fontSize = 12.0f;
+    CPTMutableLineStyle *axisLineStyle = [CPTMutableLineStyle lineStyle];
+    axisLineStyle.lineWidth = 2.0f;
+    axisLineStyle.lineColor = [CPTColor blackColor];
+    CPTMutableTextStyle *axisTextStyle = [[CPTMutableTextStyle alloc] init];
+    axisTextStyle.color = [CPTColor whiteColor];
+    axisTextStyle.fontName = @"Helvetica-Bold";
+    axisTextStyle.fontSize = 11.0f;
+    CPTMutableLineStyle *tickLineStyle = [CPTMutableLineStyle lineStyle];
+    tickLineStyle.lineColor = [CPTColor blackColor];
+    tickLineStyle.lineWidth = 2.0f;
+    CPTMutableLineStyle *gridLineStyle = [CPTMutableLineStyle lineStyle];
+    tickLineStyle.lineColor = [CPTColor blackColor];
+    tickLineStyle.lineWidth = 1.0f;
+    // 2 - Get axis set
+    CPTXYAxisSet *axisSet = (CPTXYAxisSet *) hostView.hostedGraph.axisSet;
+    // 3 - Configure x-axis
+    CPTAxis *x = axisSet.xAxis;
+    CPTAxis *y = axisSet.yAxis;
+    y.title = @"Usage (Watt)";
+    y.titleTextStyle = axisTitleStyle;
+    y.titleOffset = -40.0f;
+    y.axisLineStyle = axisLineStyle;
+    y.majorGridLineStyle = gridLineStyle;
+    y.labelingPolicy = CPTAxisLabelingPolicyNone;
+    y.labelTextStyle = axisTextStyle;
+    y.labelOffset = 16.0f;
+    y.majorTickLineStyle = axisLineStyle;
+    y.majorTickLength = 4.0f;
+    y.minorTickLength = 2.0f;
+    y.tickDirection = CPTSignPositive;
+    NSInteger majorIncrement = 100;
+    NSInteger minorIncrement = 50;
+    CGFloat yMax = 2000.0f;
+    NSMutableSet *yLabels = [NSMutableSet set];
+    NSMutableSet *yMajorLocations = [NSMutableSet set];
+    NSMutableSet *yMinorLocations = [NSMutableSet set];
+    for (NSInteger j = minorIncrement; j <= yMax; j += minorIncrement) {
+        NSUInteger mod = j % majorIncrement;
+        if (mod == 0) {
+            CPTAxisLabel *label = [[CPTAxisLabel alloc] initWithText:[NSString stringWithFormat:@"%i", j] textStyle:y.labelTextStyle];
+            NSDecimal location = CPTDecimalFromInteger(j);
+            label.tickLocation = location;
+            label.offset = -y.majorTickLength - y.labelOffset;
+            if (label) {
+                [yLabels addObject:label];
+            }
+            [yMajorLocations addObject:[NSDecimalNumber decimalNumberWithDecimal:location]];
+        } else {
+            [yMinorLocations addObject:[NSDecimalNumber decimalNumberWithDecimal:CPTDecimalFromInteger(j)]];
+        }
+    }
+    y.axisLabels = yLabels;    
+    y.majorTickLocations = yMajorLocations;
+    y.minorTickLocations = yMinorLocations;
+    
+    for (unsigned int i = 0; i < graphDataDictionary.count; i++)
+    {
+        NSDictionary *graph = [graphDataDictionary objectAtIndex:i];
+        NSNumber *timestamp = [NSNumber numberWithUnsignedInt: [[graph objectForKey:@"timestamp"] intValue]];
+        NSTimeInterval _interval = timestamp.doubleValue - (60*60)*2;
+        NSDate *date = [NSDate dateWithTimeIntervalSince1970:_interval];
+        NSDateFormatter *_formatter= [[NSDateFormatter alloc] init];
+        [_formatter setDateFormat:@"hh:mm:ss"];
+        [xAxisDateLabels addObject:[_formatter stringFromDate:date]];
+    }
+    
+    x.labelingPolicy = CPTAxisLabelingPolicyNone; // This allows us to create custom axis labels for x axis
+    NSMutableArray *ticks = [NSMutableArray arrayWithCapacity:1];
+    for(unsigned int counter = 0; counter < [xAxisDateLabels count];counter++) {
+        // Here the instance variable _axisLabelStrings is a list of custom labels
+        [ticks addObject:[NSNumber numberWithInt:counter]];
+    }
+    NSUInteger labelLocation = 0;
+    NSMutableArray* customLabels = [NSMutableArray arrayWithCapacity:[xAxisDateLabels count]];
+    @try {
+        for (NSNumber *tickLocation in ticks) {
+            CPTAxisLabel *newLabel = [[CPTAxisLabel alloc] initWithText: [xAxisDateLabels objectAtIndex:labelLocation++] textStyle:x.labelTextStyle];
+            newLabel.tickLocation = [tickLocation decimalValue];
+            newLabel.offset = x.labelOffset + x.majorTickLength;
+            newLabel.rotation = M_PI/3.5f;
+            [customLabels addObject:newLabel];
+        }
+    }
+    @catch (NSException * e) {
+        NSLog(@"An exception occurred while creating date labels for x-axis");
+    }
+    @finally {
+        x.axisLabels =  [NSSet setWithArray:customLabels];
+    }
+    x.majorTickLocations = [NSSet setWithArray:ticks];
+    
+    hostView.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
+    
+    //hostView.allowPinchScaling = NO;
+    
+    [plotSpace scaleToFitPlots:[graph allPlots]];
 }
 
 - (void)didReceiveMemoryWarning
@@ -69,24 +179,22 @@
 // This method is here because this class also functions as datasource for our graph
 // Therefore this class implements the CPTPlotDataSource protocol
 - (NSUInteger)numberOfRecordsForPlot:(CPTPlot *)plotnumberOfRecords {
-    return 10; // Our sample graph contains 9 'points'
+    return graphDataDictionary.count;
 }
 
 // This method is here because this class also functions as datasource for our graph
 // Therefore this class implements the CPTPlotDataSource protocol
 - (NSNumber *)numberForPlot:(CPTPlot *)plot field:(NSUInteger)fieldEnum recordIndex:(NSUInteger)index
 {
-    // We need to provide an X or Y (this method will be called for each) value for every index
-    int x = index;
-    
     // This method is actually called twice per point in the plot, one for the X and one for the Y value
     if(fieldEnum == CPTScatterPlotFieldX)
     {
-        // Return x value, which will, depending on index, be between -4 to 4
-        return [NSNumber numberWithInt: x];
+        // Return the x-value of the current point.
+        return [NSNumber numberWithInt:index];
     } else {
-        // Return y value, for this example we'll be plotting y = x * x
-        return [NSNumber numberWithInt: x * 0.5];
+        // Return the x-value of the current point.
+        NSDictionary *graph = [graphDataDictionary objectAtIndex:index];
+        return [NSNumber numberWithFloat: [[graph objectForKey:@"consact"] floatValue] * 1000.0f];
     }
 }
 
@@ -97,7 +205,7 @@
     NSError *error;
     NSMutableDictionary *energyData = [NSJSONSerialization
                                        JSONObjectWithData:jsonData
-                                       options:NSJSONReadingMutableContainers
+                                       options:NSJSONReadingMutableLeaves
                                        error:&error];
 
     if (error)
